2252bb71f3e61e2d1730fabb0fa9a40fdbc7a62c
manoelfilho <manoel.tsf@gmail.com>
2022-02-13 10:43:22 -0300

    comments about Model extensions

--- Domain/Models/Model.swift
+++ Domain/Models/Model.swift
@@ -2,6 +2,8 @@ import Foundation
 
 public protocol Model: Codable, Equatable {}
 
+
+//Registrar as extensoes de todos os models
 public extension Model {
     func toData() -> Data? {
         return try? JSONEncoder().encode(self)
