c7381d3bf71ea0ea5a314a118ab08b10645f6a24
manoelfilho <manoel.tsf@gmail.com>
2022-02-13 10:42:27 -0300

    unified spy of HttpClient in unique file

---
+++ DataTests/Http/HttpClientSpy.swift
@@ -0,0 +1,26 @@
+import Foundation
+import Domain
+import Data
+
+//Simula um HttpPost. É um mok de alguma classe que nao existe
+public class HttpClientSpy: HttpPostClient {
+    var urls = [URL]()
+    var data: Data?
+    var completion: ((Result<Data, HttpError>) -> Void)?
+
+    public func post(to url: URL, with data: Data?, completion: @escaping (Result<Data, HttpError>) -> Void) {
+        self.urls.append(url)
+        self.data = data
+        self.completion = completion
+    }
+
+    /*
+        Simula completions / closures com erros
+     */
+    func completeWithError( _ error: HttpError){
+        completion?(.failure(error))
+    }
+    func completeWithData( _ data: Data){
+        completion?(.success(data))
+    }
+}
