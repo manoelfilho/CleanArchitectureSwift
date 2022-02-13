810ea8065ee3f925a9d6f60df33651e5200b52b5
manoelfilho <manoel.tsf@gmail.com>
2022-02-13 11:26:24 -0300

    add infra layer

---
+++ InfraTests/AlamofireAdapterTests.swift
@@ -0,0 +1,14 @@
+import XCTest
+
+/*
+    No módulo Data/Http criamos um HttpPostClient que é um protocolo que diz como deve ser feita as requisicoes Post. Ao mesmo tempo, usaremos uma biblioteca de terceiro (Alamofire) que tem os seus recursos de requisicoes. O Adapter é uma intermediacao entre esses dois. AlamofireAdapterTests serve para testar esse adapter a ser criado
+                        
+ */
+
+class AlamofireAdapterTests: XCTestCase {
+
+    func test_ (){
+        
+    }
+
+}
