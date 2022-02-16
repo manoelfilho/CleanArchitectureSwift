88eae6b2711052c9093a4ff519035cef3ce094f9
manoelfilho <manoel.tsf@gmail.com>
2022-02-16 00:54:16 -0300

    moving AlamofireAdapter to its own file and folder

--- InfraTests/AlamofireAdapterTests.swift
+++ InfraTests/AlamofireAdapterTests.swift
@@ -7,45 +7,7 @@ import XCTest
 import Alamofire
 import Data
 import Domain
-
-class AlamoFireAdapter: HttpPostClient {
-    
-    private let session: Session
-    
-    init(session: Session = .default){
-        self.session = session
-    }
-    
-    func post(to url: URL, with data: Data?, completion: @escaping (Result<Data?, HttpError>) -> Void){
-        session.request(url, method: .post, parameters: data?.toJson(), encoding: JSONEncoding.default).responseData { dataResponse in
-            
-            guard let statusCode = dataResponse.response?.statusCode else {
-                return completion(.failure(.noConnectivity))
-            }
-            
-            switch dataResponse.result {
-            case .failure: completion(.failure(.noConnectivity))
-            case .success(let data):
-                switch statusCode {
-                case 204:
-                    completion(.success(nil))
-                case 200...299:
-                    completion(.success(data))
-                case 401:
-                    completion(.failure(.unauthorized))
-                case 403:
-                    completion(.failure(.forbiden))
-                case 400...499:
-                    completion(.failure(.badRequest))
-                case 500...599:
-                    completion(.failure(.serverError))
-                default:
-                    completion(.failure(.noConnectivity))
-                }
-            }
-        }
-    }
-}
+import Infra
 
 class AlamofireAdapterTests: XCTestCase {
 
@@ -125,13 +87,13 @@ class AlamofireAdapterTests: XCTestCase {
  */
 extension AlamofireAdapterTests {
     
-    func makeSut(file: StaticString = #filePath, line: UInt = #line) -> AlamoFireAdapter {
+    func makeSut(file: StaticString = #filePath, line: UInt = #line) -> AlamofireAdapter {
         //definimos uma configuracao customizada para o Session do Alamofire.
         //assim ele não faz a conexao padrao e sim uma fake
         let configuration = URLSessionConfiguration.default
         configuration.protocolClasses = [UrlProtocolStub.self]
         let session = Session(configuration: configuration)
-        let sut = AlamoFireAdapter(session: session)
+        let sut = AlamofireAdapter(session: session)
         checkMemoryLeak(for: sut, file: file, line: line)
         return sut
     }
