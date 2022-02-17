75577e1fa192ece5ad865a88a22da1963d414af2
manoelfilho <manoel.tsf@gmail.com>
2022-02-17 19:07:07 -0300

    move presentations classes to theirs files

---
+++ Presenter/Presenters/SignupPresenter.swift
@@ -0,0 +1,54 @@
+//
+//  SignupPresenter.swift
+//  Domain
+//
+//  Created by Manoel Filho on 17/02/22.
+//
+
+import Foundation
+
+public struct SignupViewModel {
+    public var confirmed: Bool?
+    public var blocked: Bool?
+    public var username: String?
+    public var email: String?
+    public var password: String?
+    public var role: Int?
+    
+    public init(confirmed: Bool? = nil, blocked: Bool? = nil, username: String? = nil, email: String? = nil, password: String? = nil, role: Int? = nil){
+        self.confirmed = confirmed
+        self.blocked = blocked
+        self.username = username
+        self.email = email
+        self.password = password
+        self.role = role
+    }
+}
+
+public class SignupPresenter {
+    
+    private let alertView: AlertView
+    
+    public init(alertView: AlertView){
+        self.alertView = alertView
+    }
+    
+    public func signUp(viewModel: SignupViewModel){
+        if let message = validate(viewModel: viewModel) {
+            alertView.showMessage(viewModel: AlertViewModel(title: "Falha", message: message))
+        }
+    }
+    
+    func validate(viewModel: SignupViewModel) -> String? {
+        if viewModel.username == nil || viewModel.username!.isEmpty {
+            return "O campo username é obrigatório"
+        }
+        if viewModel.email == nil || viewModel.email!.isEmpty {
+            return "O campo email é obrigatório"
+        }
+        if viewModel.password == nil || viewModel.password!.isEmpty {
+            return "O campo password é obrigatório"
+        }
+        return nil
+    }
+}
--- PresenterTests/Presenters/SignupPresenterTests.swift
+++ PresenterTests/Presenters/SignupPresenterTests.swift
@@ -6,52 +6,7 @@
 //
 
 import XCTest
-
-class SignupPresenter {
-    
-    private let alertView: AlertView
-    
-    init(alertView: AlertView){
-        self.alertView = alertView
-    }
-    
-    func signUp(viewModel: SignupViewModel){
-        if let message = validate(viewModel: viewModel) {
-            alertView.showMessage(viewModel: AlertViewModel(title: "Falha", message: message))
-        }
-    }
-    
-    func validate(viewModel: SignupViewModel) -> String? {
-        if viewModel.username == nil || viewModel.username!.isEmpty {
-            return "O campo username é obrigatório"
-        }
-        if viewModel.email == nil || viewModel.email!.isEmpty {
-            return "O campo email é obrigatório"
-        }
-        if viewModel.password == nil || viewModel.password!.isEmpty {
-            return "O campo password é obrigatório"
-        }
-        return nil
-    }
-}
-
-protocol AlertView {
-    func showMessage(viewModel: AlertViewModel)
-}
-
-struct AlertViewModel: Equatable {
-    var title: String
-    var message: String
-}
-
-struct SignupViewModel {
-    var confirmed: Bool?
-    var blocked: Bool?
-    var username: String?
-    var email: String?
-    var password: String?
-    var role: Int?
-}
+import Presenter
 
 class SignupPresenterTests: XCTestCase {
 
