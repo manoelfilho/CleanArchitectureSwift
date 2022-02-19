b88350dbd9311c68e948620de765865c881e9b39
manoelfilho <manoel.tsf@gmail.com>
2022-02-19 10:04:59 -0300

    move LoadingViewSpy to its file

---
+++ PresenterTests/Views/LoadingViewSpy.swift
@@ -0,0 +1,27 @@
+//
+//  LoadingViewSpy.swift
+//  PresenterTests
+//
+//  Created by Manoel Filho on 19/02/22.
+//
+
+import Foundation
+//
+//  LoadingViewSpy.swift
+//  PresenterTests
+//
+//  Created by Manoel Filho on 19/02/22.
+//
+
+import Foundation
+import Presenter
+
+class LoadingViewSpy: LoadingView {
+    var emit: ((LoadingViewModel) -> Void)?
+    func observe(completion: @escaping (LoadingViewModel) -> Void){
+        self.emit = completion
+    }
+    func display(viewModel: LoadingViewModel) {
+        self.emit?(viewModel)
+    }
+}
