2771961cb2ff244da0777a90f2ac265f4d2aaf95
manoelfilho <manoel.tsf@gmail.com>
2022-02-19 01:40:56 -0300

    move SignupPresenter files to its own files

--- Presenter/Presenters/Signup/SignupPresenter.swift
+++ Presenter/Presenters/Signup/SignupPresenter.swift
@@ -9,26 +9,6 @@ import Foundation
 import Domain
 import XCTest
 
-public struct SignupViewModel {
-    
-    public var confirmed: Bool?
-    public var blocked: Bool?
-    public var username: String?
-    public var email: String?
-    public var password: String?
-    public var role: Int?
-    
-    public init(confirmed: Bool? = nil, blocked: Bool? = nil, username: String? = nil, email: String? = nil, password: String? = nil, role: Int? = nil){
-        self.confirmed = confirmed
-        self.blocked = blocked
-        self.username = username
-        self.email = email
-        self.password = password
-        self.role = role
-    }
-    
-}
-
 public class SignupPresenter {
     
     private let alertView: AlertView
---
+++ Presenter/Presenters/Signup/SignupViewModel.swift
@@ -0,0 +1,28 @@
+//
+//  SignupViewModel.swift
+//  Presenter
+//
+//  Created by Manoel Filho on 19/02/22.
+//
+
+import Foundation
+
+public struct SignupViewModel {
+    
+    public var confirmed: Bool?
+    public var blocked: Bool?
+    public var username: String?
+    public var email: String?
+    public var password: String?
+    public var role: Int?
+    
+    public init(confirmed: Bool? = nil, blocked: Bool? = nil, username: String? = nil, email: String? = nil, password: String? = nil, role: Int? = nil){
+        self.confirmed = confirmed
+        self.blocked = blocked
+        self.username = username
+        self.email = email
+        self.password = password
+        self.role = role
+    }
+    
+}
