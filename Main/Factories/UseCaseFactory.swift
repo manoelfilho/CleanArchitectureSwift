6020d0b3d417f0d907272a8f450c78d7ff93fe2b
manoelfilho <manoel.tsf@gmail.com>
2022-02-24 12:52:05 -0300

    improve code readbility

--- Main/Factories/UseCaseFactory.swift
+++ Main/Factories/UseCaseFactory.swift
@@ -5,9 +5,15 @@ import Domain
 
 
 final class UseCaseFactory {
+    
+    private static let httpClient = AlamofireAdapter()
+    private static let baseUrl = K.api_url
+    
+    private static func makeUrl(path: String) -> URL{
+        return URL(string: "\(baseUrl)/\(path)")!
+    }
+    
     static func makeRemoteAddAccount() -> AddAccount {
-        let alamofireAdapter = AlamofireAdapter()
-        let url = URL(string: "\(K.api_url)/users")!
-        return RemoteAddAccount(url: url, httpClient: alamofireAdapter)
+        return RemoteAddAccount(url: makeUrl(path: "signup"), httpClient: httpClient)
     }
 }
